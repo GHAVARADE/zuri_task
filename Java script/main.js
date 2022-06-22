719dd786a33284e4ab662847c43b93a0821424fb
GHAVARADE <abdulgafaradekunjo@gmail.com>
2022-06-22 13:03:19 +0100

    print name

---
+++ Java script/main.js
@@ -0,0 +1,6 @@
+let name = "Adekunjo abdulgafar Adetunji";
+let height = "5.8ft";
+let country = "Nigeria";
+document.writeln("My name is " + name);
+document.writeln('I am '+height+" tall");
+document.writeln("I am from "+country)
